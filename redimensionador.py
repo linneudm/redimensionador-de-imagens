e8b7d51a251b6a063b3f65ce4f2711d0c7957753
Linneu Lopes <linneu.dm@gmail.com>
2018-03-06 00:33:01 -0300

    Update redimensionador.py

--- redimensionador.py
+++ redimensionador.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import os
-
+import pathlib
 #Obtem o diretorio atual. Supoe-se que o script esta na mesma pasta das imagens para serem redimensionadas.
-directory = os.fsencode(os.getcwd())
+directory = os.path.dirname(os.path.abspath(__file__))
 print(directory)
 
 w = int(input("Insira a largura da imagem (cm): "))
@@ -11,17 +11,14 @@ h = int(input("Insira a altura da imagem (cm): "))
 w_px = int(37.795 * w)
 h_px = int(37.795 * h)
 size = w_px, h_px
-list_formats = ("jpg", "png", "jpeg")
+list_formats = (".jpg", ".png", ".jpeg")
 for i,file in enumerate(os.listdir(directory)):
     is_image = False
-    #Obtem o arquivo em STR
-    file_format = str(file, 'utf-8')
-    #Obtem o nome do arquivo
-    file_name = file_format.split(".")[0]
-    #Obtem o formato do arquivo
-    file_format = file_format.split(".")[-1]
+    #Obtem o nome do arquivo e o formato e coloca-os em uma lista ("nome", "formato")
+    file_name = os.path.splitext(file)
+    #print(">>", file_name[1])
     for f in list_formats:
-        if(file_format == f):
+        if(file_name[1] == f):
             #Verifica se o formato do arquivo pode ser convertido. Se sim, encerra a verificacao.
             is_image = True
             break
@@ -31,12 +28,9 @@ for i,file in enumerate(os.listdir(directory)):
         #Redimensiona a imagem para o tamanho escolhido
         im_resized = im.resize(size, Image.ANTIALIAS)
         #Verifica se existe a pasta "imgs". Se nao, ele cria.
-        try:
-            os.stat("imgs")
-        except:
-            os.mkdir("imgs") 
+        pathlib.Path('/imgs/').mkdir(exist_ok=True) 
         #Salva a imagem.
-        im_resized.save("imgs/reduzido_{}.{}".format(file_name,file_format))
+        im_resized.save("imgs/redimensionado_{}{}".format(file_name[0],file_name[1]))
     else:
         #Imprime mensagem dos formatos que encontrou e nao foram suportados.
-        print("O formato .{} nao e suportado.".format(file_format))
+        print("O formato do arquivo {}{} nao e suportado.".format(file_name[0],file_name[1]))
