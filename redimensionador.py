48ffb212a5c1c52698210e36d0d14017130cfe7c
Linneu Lopes <linneu.dm@gmail.com>
2018-03-13 14:31:17 -0300

    Add imagens para PDF
    Adicionando as imagens redimensionadas a um PDF

--- redimensionador.py
+++ redimensionador.py
@@ -1,15 +1,18 @@
 from PIL import Image
+from fpdf import FPDF
 import os
 import pathlib
+import time
 #Obtem o diretorio atual. Supoe-se que o script esta na mesma pasta das imagens para serem redimensionadas.
 directory = os.path.dirname(os.path.abspath(__file__))
-print(directory)
-
+pdf = FPDF(format="A4")
+pdf.set_compression(False)
+result = pathlib.Path('imgs_reduzidas/').mkdir(exist_ok=True)
 w = int(input("Insira a largura da imagem (cm): "))
 h = int(input("Insira a altura da imagem (cm): "))
 #Converte CM para Pixels
-w_px = int(37.795 * w)
-h_px = int(37.795 * h)
+w_px = int(38 * w)
+h_px = int(38 * h)
 size = w_px, h_px
 list_formats = (".jpg", ".png", ".jpeg")
 for i,file in enumerate(os.listdir(directory)):
@@ -28,9 +31,39 @@ for i,file in enumerate(os.listdir(directory)):
         #Redimensiona a imagem para o tamanho escolhido
         im_resized = im.resize(size, Image.ANTIALIAS)
         #Verifica se existe a pasta "imgs". Se nao, ele cria.
-        pathlib.Path('/imgs/').mkdir(exist_ok=True) 
         #Salva a imagem.
-        im_resized.save("imgs/redimensionado_{}{}".format(file_name[0],file_name[1]))
+        im_resized.save("imgs_reduzidas/redimensionado_{}{}".format(file_name[0],file_name[1]))
     else:
         #Imprime mensagem dos formatos que encontrou e nao foram suportados.
         print("O formato do arquivo {}{} nao e suportado.".format(file_name[0],file_name[1]))
+#X e Y sao as margens da folha
+x, y = 10,10
+#j verifica se existem 2 elementos na mesma linha para fazer o espacamento entre as imagens
+j = 0
+#adapta o tamanho das imagens para o tamanho da folha
+w_px = w_px/3.7
+h_px = h_px/3.7
+#percorre as imagens dentro do diretorio criado
+for i,image in enumerate(os.listdir('imgs_reduzidas')):
+    #So serao criado 6 elementos por folha
+    if(i % 6 == 0):
+        x, y = 10,10
+        j=0
+        #Adiciona uma nova pagina
+        pdf.add_page()
+    #Verifica se existem 2 elementos na mesma linha. Se sim, salta para proxima linha.
+    if (j == 2):
+        j=0
+        y += h_px + 4
+        pdf.image('imgs_reduzidas/{}'.format(image),x,y,w_px,h_px)
+        j+=1
+    else:
+        #Verifica se eh o primeiro elemento da linha.
+        if(j==0):
+            pdf.image('imgs_reduzidas/{}'.format(image),x,y,w_px,h_px)
+        else:
+        #Caso nao, faz o espacamento horizontal.
+            pdf.image('imgs_reduzidas/{}'.format(image),x+w_px+4,y,w_px,h_px)
+        j+=1
+#Cria o PDF na pasta raiz com o nome indicado
+pdf.output("imagens_reduzidas.pdf", "F")
